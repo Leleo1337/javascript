15d47469805586c3fa74f1e64486a76ce35d956e
Leleo1337 <leandroroncatto18@gmail.com>
2024-07-01 18:30:05 -0300

    exercicios

---
+++ exercicios/modulo D/ex009.js
@@ -0,0 +1,7 @@
+var vel = 61
+console.log(`sua velocidade é de ${vel}km/h`)
+
+if (vel > 60){
+    console.log(`você foi multado por exesso de velocidade`)
+}
+console.log(`dirija sempre com cinto de segurança!`)
